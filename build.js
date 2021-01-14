0b86cb8da221a2f041e7f7c0d28630f708f15aa3
Mohamed Mahrous <m.mahrous.94@gmail.com>
2021-01-15 01:42:30 +0200

    update build script

--- build.js
+++ build.js
@@ -1,7 +1,32 @@
 const { exec } = require("child_process");
-const { version } = require('./package.json');
+const { version, name } = require('./package.json');
+const filename = `${name}_v${version}`;
 
-exec(`pkg index.js --out-path build/v${version} --public`, (error, stdout, stderr) => {
+exec(`pkg index.js --output ./bin/v${version}/${filename}_mac_x64 --targets node14-macos-x64 --public`, (error, stdout, stderr) => {
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+});
+
+exec(`pkg index.js --output ./bin/v${version}/${filename}_linux_x64 --targets node14-linux-x64 --public`, (error, stdout, stderr) => {
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+});
+
+exec(`pkg index.js --output ./bin/v${version}/${filename}_win_x64 --targets node14-win-x64 --public`, (error, stdout, stderr) => {
 	if (error) {
 		console.log(`error: ${error.message}`);
 		return;
